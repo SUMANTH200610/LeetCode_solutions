5e37dc6bc45b8c0f9d04c40834bd80503b35e289
SUMANTH200610 <aravasatyasumanth710@gmail.com>
2026-08-17 14:36:56 +0530

    Time: 1511 ms (5.62%), Space: 134.2 MB (82.85%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+    vector<int>a(n);
+    int lefty = 0,left = 0;
+    long long sum = 0;
+    for(int i = k;i<n;i++){
+        if(k+k < n){
+        if((i-k> i+k-left)) break;
+        if (i-k == i+k-left){
+            sum += nums[i-k];
+            break;
+        }
+        sum += nums[i-k]+nums[i+k-left];
+        cout << sum << " ";
+        left += 2;
+    }
+    else break;
+    }
+    cout << sum << endl;
+    
+    for(int i = 0;i<n;i++){
+        if(i-k <0 || i+k>= n) a[i] = -1;
+        else{
+            int avg = sum/(i+k-lefty+1);
+            cout << avg << endl;
+            a[i] = avg;
+            sum -= nums[lefty];
+            lefty++;
+           if(i+k+1 < n) sum += nums[i+k+1];
+
+        }
+    }
+    return a;
+ //   return {};
+    
+    }
+};
