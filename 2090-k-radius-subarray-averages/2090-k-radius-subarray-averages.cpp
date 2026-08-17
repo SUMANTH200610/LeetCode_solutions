007e1f731c78ee5bfd531d6b26d474d685c6c4c9
SUMANTH200610 <aravasatyasumanth710@gmail.com>
2026-08-17 15:48:45 +0530

    Time: 14 ms (10.86%), Space: 134.3 MB (63.84%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -2,38 +2,20 @@ class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n = nums.size();
-    vector<int>a(n);
-    int lefty = 0,left = 0;
-    long long sum = 0;
-    for(int i = k;i<n;i++){
-        if(k+k < n){
-      //  if((i-k> i+k-left)) break;
-        if (i-k == i+k-left){
-            sum += nums[i-k];
-            break;
+        vector<int>a(n,-1);
+        long long sum = 0;
+        if(2*k+1 > n) return a;
+        for(int i = 0;i<=2*k;i++){
+            sum+= nums[i];
         }
-        sum += nums[i-k]+nums[i+k-left];
-        cout << sum << " ";
-        left += 2;
-    }
-    else break;
-    }
-    cout << sum << endl;
-    
-    for(int i = 0;i<n;i++){
-        if(i-k <0 || i+k>= n) a[i] = -1;
-        else{
-            int avg = sum/(i+k-lefty+1);
-            cout << avg << endl;
-            a[i] = avg;
-            sum -= nums[lefty];
-            lefty++;
-           if(i+k+1 < n) sum += nums[i+k+1];
-
+        a[k] = sum/(2*k+1);
+        int j = 0;
+        for(int i = k+1;i<n-k;i++){
+            sum += nums[i+k];
+            sum -= nums[i-k-1];
+            j++;
+            a[i] = sum/(2*k+1);
         }
-    }
-    return a;
- //   return {};
-    
+        return a;
     }
 };
