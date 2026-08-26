ade3c23345d5e42bd0ed33207e0bd6c0cba38f4f
SUMANTH200610 <aravasatyasumanth710@gmail.com>
2026-08-26 18:20:34 +0530

    Time: 3 ms (0.48%), Space: 15.2 MB (43.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size()-1;
-       int  mid = (low+high)/2;
-        if(target == nums[mid]) return mid;
-       if(target > nums[mid]){
-        for(int i = mid+1;i<=high;i++){
-            if(target == nums[i]) return i;
-           else if(nums[i]>target) return i;
+        unordered_map<int,int>m;
+        for(int i: nums){
+            m[i]++;
         }
-          //  return nums.size();
+        if(m.find(target) == m.end()){
+            for(int i = 0;i<nums.size();i++){
+                if(target< nums[i]){
+                    target = nums[i];
+                    break;
+                }
+
+            }
+        }
+        if(target > nums[nums.size()-1]) return nums.size();
+            int low = 0;
+            int high = nums.size()-1;
+            while(low<= high){
+                int mid = (low+high)/2;
+                if(nums[mid] == target) return mid;
+                else if(nums[mid]> target){
+                    high = mid-1;
+
+                }
+                else low = mid+1;
+            }
         
-    
-       }
-       else {
-        for(int i = low;i<mid;i++){
-            if(target == nums[i]) return i;
-      else if (nums[i]>target) return i;
-        }
-       return mid;
-       }
-    return nums.size();
+        return 0;
     }
 };
