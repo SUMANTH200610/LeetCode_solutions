f619416383529b4450d56e20ed9ebd755d268dd9
SUMANTH200610 <aravasatyasumanth710@gmail.com>
2026-09-15 06:25:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (69.26%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     string decodeString(string s) {
-        //vector<char>a;
-        stack<int>st;
-        stack<string>sta;
+        stack<int> nums;
+        stack<string>str;
         int num = 0;
         string current = "";
 
         for(int i = 0;i<s.size();i++){
-         if(isdigit(s[i])){
-            num = num*10 + s[i]-'0';
-         }
-         else if(s[i] == '['){
-           st.push(num);
-           sta.push(current);
-           num = 0;
-           current = "";
-
-         }
-         else if(s[i] == ']'){
-            int repeat = st.top();
-            st.pop();
-            string prev = sta.top();
-            sta.pop();
-            string temp = "";
-            for(int i = 0;i<repeat;i++){
-                temp += current;
+            if(isdigit(s[i])){
+                num = num*10+s[i]-'0';
             }
-            current = prev+temp;
-         }
-         
-          else{
-             current+= s[i];
+            else if(s[i] == '['){
+                nums.push(num);
+                num = 0;
+                str.push(current);
+                current = "";
 
-         }
-          }
-        
+            }
+            else if(s[i] == ']'){
+                int dupli = nums.top();
+                nums.pop();
+                string prev = str.top();
+                str.pop();
+               string temp = "";
+                for(int i = 0;i<dupli;i++){
+                    temp += current;
+                }
+                current = prev+temp;
+
+            }
+            else{
+                current += s[i];
+            }
+        }
         return current;
     }
 };
