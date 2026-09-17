d07f20aeee3aa82a71f93e5191bcfacd200f98a8
SUMANTH200610 <aravasatyasumanth710@gmail.com>
2026-09-17 20:49:13 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.17%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int sum = 0;
-        string str = "";
-        for(int i = 0;i<s.size();i++){
-            if(s[i] == '(') {
-                sum++;
-                if(sum > 1) str.push_back(s[i]);
+        stack<char>st;
+        string ans = "";
+        for(char c:s){
+            if(c == '('){
+                if(!st.empty()){
+                    ans.push_back(c);
+                }
+                st.push(c);
             }
-            
-           // if(sum == 1 || sum == 0) continue;
             else{
-                sum--;
-                if(sum>=1){
-            str.push_back(s[i]);
-            }
-            
+                st.pop();
+                if(!st.empty()){
+                    ans.push_back(c);
+                }
 
+            }
         }
-        }
-        return str;
+        return ans;
     }
 };
